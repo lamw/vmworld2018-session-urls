1846b84dc75687a32ecb8c2403b30063679e4c49
William Lam <william2003@gmail.com>
2018-09-03 17:59:22 -0700

    PS Script to download sessions

---
+++ downloadSessions.ps1
@@ -0,0 +1,14 @@
+$sessionsToDownloadFile = "us.txt"
+
+$lines = Get-Content -Path $sessionsToDownloadFile | Where-Object { $_.Trim() -ne '' }
+
+$headers = @{"referer" = "http://www.vmware.com"}
+
+foreach ($line in $lines) {
+    ($title,$url) = $line -split "#"
+    $title = $title + ".mp4"
+    Write-Host "Downloading $title ..."
+    Invoke-WebRequest -Uri $url -Headers $headers -Outfile $url
+}
+
+
