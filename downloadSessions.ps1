3588380314fed39d6bd07904fad6951835161b07
William Lam <william2003@gmail.com>
2018-09-04 09:04:45 -0700

    Removing special characters & trailing spaces from titles

--- downloadSessions.ps1
+++ downloadSessions.ps1
@@ -6,6 +6,9 @@ $headers = @{"referer" = "http://www.vmware.com"}
 
 foreach ($line in $lines) {
     ($title,$url) = $line -split "#"
+    $title = $title.replace("`:",'')
+    $title = $title.replace("`'",'')
+    $title = $title.trim()
     $title = $title + ".mp4"
     Write-Host "Downloading $title ..."
     Invoke-WebRequest -Uri $url -Headers $headers -Outfile $url
